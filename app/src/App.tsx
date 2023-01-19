c426152be5092a8b00a9e7ffc496f5de4f3d08bc
pp <ichikawa929@yahoo.co.jp>
2023-01-20 03:42:19 +0900

    feat: Add feature make playlist for spotify

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,11 +3,13 @@ import { Home } from "./screens/Home";
 import { SpecifyTime } from "./screens/SpecifyTime";
 import { Footer } from "./components/Footer";
 import { OAuthButton } from "./components/OAuthButton";
+import { SpecifyForm } from "./components/SpecifyForm";
 
 export const App = () => {
   return (
     <div>
       <OAuthButton />
+      <SpecifyForm/>
       <Header />
       <Home />
       <SpecifyTime />
