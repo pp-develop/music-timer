d8d87f48b3bad5a91cd96217f1dc61b268e7cffd
pp <ichikawa929@yahoo.co.jp>
2023-02-19 22:06:28 +0900

    feat: add react-native-dotenv

--- app/src/config/index.ts
+++
@@ -1,3 +0,0 @@
-// export const API_URL = process.env.API_URL as string;
-export const API_URL = "http://localhost:8080"
-export const JWT_SECRET = '123456' as string;
--- app/src/lib/axos.ts
+++ app/src/lib/axos.ts
@@ -1,7 +1,6 @@
-import Axios, { AxiosRequestConfig } from 'axios';
-
-import { API_URL } from '../config';
+import Axios from 'axios';
+import { BASE_URL } from '@env'
 
 export const axios = Axios.create({
-  baseURL: API_URL,
+  baseURL: BASE_URL,
 });
