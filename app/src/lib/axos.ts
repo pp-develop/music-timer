07187b988c08ac8088df9f9c02ee4ec34517b0a6
pp <ichikawa929@yahoo.co.jp>
2023-02-19 23:32:36 +0900

    refactor: reference api base

--- app/src/lib/axos.ts
+++ app/src/lib/axos.ts
@@ -1,6 +1,15 @@
 import Axios from 'axios';
-import { BASE_URL } from '@env'
+import { API_URL } from '../config';
 
 export const axios = Axios.create({
-  baseURL: BASE_URL,
+  baseURL: API_URL,
 });
+
+// axios.interceptors.request.use(response => {
+//   // 同様にresponseをreturnする必要がある
+//   console.log("test")
+//   return response
+// }, err => {
+//   // 何らかのエラー処理
+//   return Promise.reject(err)
+// })
