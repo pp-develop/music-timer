8826c4e12f10e7ebbfcc67033972a99cbaab8313
pp <ichikawa929@yahoo.co.jp>
2023-02-19 23:33:10 +0900

    fix: Delete unnecessary code

--- app/src/lib/axos.ts
+++ app/src/lib/axos.ts
@@ -4,12 +4,3 @@ import { API_URL } from '../config';
 export const axios = Axios.create({
   baseURL: API_URL,
 });
-
-// axios.interceptors.request.use(response => {
-//   // 同様にresponseをreturnする必要がある
-//   console.log("test")
-//   return response
-// }, err => {
-//   // 何らかのエラー処理
-//   return Promise.reject(err)
-// })
