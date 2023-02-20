d0557d339eafd5880681b935b1b40e9c70f4e550
pp <ichikawa929@yahoo.co.jp>
2023-02-21 03:23:51 +0900

    feat: add react-helmet-async

--- app/src/components/Layout/index.tsx
+++ app/src/components/Layout/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Header } from "../Parts/Header"
+import { Head } from "../Parts/Head"
 import { Description } from "../Parts/Description";
 import { Form } from "../../features/createPlaylist";
 import { DeletePlaylist } from "../../features/deletePlaylist/components/DeletePlaylistButton";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HelmetProvider } from 'react-helmet-async'
 
 export const App = () => {
   return (
     <SafeAreaProvider>
+      <HelmetProvider>
+        <Head />
+      </HelmetProvider>
       <Header />
       <Description />
       <Form />
