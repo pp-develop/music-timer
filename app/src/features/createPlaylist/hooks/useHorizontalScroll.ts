aceccc0945661566a698b985224acc5f60c8fec3
pp <ichikawa929@yahoo.co.jp>
2024-08-15 21:15:35 +0900

    fix: Resolve horizontal scrolling issue in artist area (#78)

--- app/src/features/createPlaylist/hooks/useHorizontalScroll.ts
+++ app/src/features/createPlaylist/hooks/useHorizontalScroll.ts
@@ -1,22 +1,55 @@
 import { useEffect, useState } from 'react';
 import { Platform } from 'react-native';
 
+
+/**
+ * カスタムフック - useHorizontalScroll
+ * 
+ * このフックは、スクロール可能な領域にマウスがホバーしているときに、縦スクロールを横スクロールに変換するためのものです。
+ * Webプラットフォーム専用です。
+ *
+ * @param {React.RefObject} scrollViewRef - スクロール対象のスクロールビューの参照
+ * @returns {Object} - `onMouseEnter` と `onMouseLeave` イベントハンドラ
+ */
 const useHorizontalScroll = (scrollViewRef) => {
+  // スクロール領域にマウスがホバーしているかどうかを管理する状態
   const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
+    // Webプラットフォーム以外ではこのフックは動作しない
     if (Platform.OS !== 'web') return;
 
+    /**
+     * スクロールイベントハンドラ
+     * 
+     * マウスのホイールイベントに基づいてスクロール動作を制御します。
+     * 縦スクロールの代わりに横スクロールを行うために使用します。
+     * 
+     * @param {WheelEvent} event - マウスのホイールイベント
+    */
     const handleWheel = (event) => {
-      if (isHovered && scrollViewRef.current) {
-        // ページ全体のスクロールを無効にする
-        event.preventDefault();
+      if (scrollViewRef.current) {
+        // 縦スクロール（deltaY）と横スクロール（deltaX）を判定
+        if (isHovered) {
+          // ページ全体のスクロールが行われることを防ぐため、
+          // ページ全体のスクロールを無効にする
+          event.preventDefault();
 
-        // 横スクロールを実行する
-        scrollViewRef.current.scrollTo({
-          x: scrollViewRef.current.scrollLeft + event.deltaY,
-          animated: false,
-        });
+          if (Math.abs(event.deltaX) > Math.abs(event.deltaY)) {
+            // 横スクロールのケース
+            scrollViewRef.current.scrollTo({
+              x: scrollViewRef.current.scrollLeft + event.deltaX,
+              animated: false,
+            });
+          } else {
+            // 縦スクロールのケース
+            // 縦スクロールを横スクロールに変換する
+            scrollViewRef.current.scrollTo({
+              x: scrollViewRef.current.scrollLeft + event.deltaY,
+              animated: false,
+            });
+          }
+        }
       }
     };
 
