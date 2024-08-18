3556721907e6a918d82cbb9e3753be2d4dc37550
pp <ichikawa929@yahoo.co.jp>
2024-08-19 02:29:04 +0900

    Add Google Analytics integration to track user interactions on web (#82)

--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -1,4 +1,3 @@
-import { setDefaultLanguage, getDefaultLanguage } from '../locales/i18n';
 import { ThemeProvider } from '../config/ThemeContext';
 import { Header } from "../components/Parts/Header"
 import { Head } from "../components/Parts/Head"
@@ -10,16 +9,6 @@ import React from "react";
 import { Slot } from 'expo-router';
 
 export default function Layout() {
-  // 現在の言語を取得
-  const currentLanguage = getDefaultLanguage();
-
-  // 言語が日本語でなければ英語をデフォルトに設定
-  if (currentLanguage !== 'ja') {
-    setDefaultLanguage('en');
-  } else {
-    setDefaultLanguage('ja');
-  }
-
   const theme = {
     primaryColor: '#D7E6EF',
     secondaryColor: '#6E777C',
